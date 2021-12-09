0ae3b9b40b80fbf5f0ea6ed78d3ab1b933e71189
D1le1 <egor20031500@gmail.com>
2021-12-09 19:52:59 +0300

    feat (Task#3) : add writeProductInFile and refactor other functions

--- SW 3/SW 3/SW 3.cpp
+++ SW 3/SW 3/SW 3.cpp
@@ -10,20 +10,22 @@ struct Product
 	int count = 0;
 };
 
-static std::string fileName = "../Text_Files/Product.txt";
 
 #pragma region DescribeFunctions
-Product* readProductFromFile();
-int getCountOfRows();
+Product* readProductFromFile(std::string, int);
+void writeProductInFile(std::string, Product*, int, int);
+int getCountOfRows(std::string);
 #pragma endregion
 
 int main()
 {
-	
+	int size = getCountOfRows("../Text_Files/Product.txt");
+	Product* products = readProductFromFile("../Text_Files/Product.txt", size);
+	writeProductInFile("../Text_Files/Products.txt",products,size,std::ios_base::out);
 }
 
 #pragma region Functions
-int getCountOfRows()
+int getCountOfRows(std::string fileName)
 {
 	std::ifstream file(fileName);
 	std::string string;
@@ -36,12 +38,11 @@ int getCountOfRows()
 	return size;
 }
 
-Product* readProductFromFile()
+Product* readProductFromFile(std::string fileName, int size)
 {
 	std::ifstream file(fileName);
 	if (!file.is_open())
 		return nullptr;
-	int size = getCountOfRows();
 	Product* products = new Product[size];
 	int index = 0;
 	while (!file.eof())
@@ -54,5 +55,17 @@ Product* readProductFromFile()
 	}
 	file.close();
 	return products;
+}
+
+void writeProductInFile(std::string fileName, Product* products, int size, int openMode)
+{
+	std::ofstream file(fileName, openMode);
+	if (!file.is_open())
+		return;
+	for (int i = 0; i < size; i++)
+	{
+		file << products[i].index << " " << products[i].cost << " " << products[i].name << " " << products[i].count << std::endl;
+	}
+	file.close();
 }
 #pragma endregion
